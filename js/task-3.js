afb0631ea9599f3ed0d54a66beb1f8499fe8b08e
Pavlo Markovsky <pash199060@gmail.com>
2025-01-22 15:05:33 +0200

    new

--- js/task-3.js
+++ js/task-3.js
@@ -5,3 +5,6 @@ function getElementWidth(content, padding, border) {
     const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
     return totalWidth;
 }
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
